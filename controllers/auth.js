7e3523ffb541086da9dffd6906fb2c1745a71841
vadym.checherinda@litslink.com <lits-user@lits-admins-MacBook-Pro.local>
2020-02-03 11:41:29 +0200

    Add login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ exports.postSignup = (req, res, next) => {
       return user.save();
     })
     .then(result => {
-      res.status(200).json({ message: "Successfully sighup" });
+      res.status(200).json({ user: result });
     })
     .catch(err => {
       const error = new Error(err);
@@ -27,26 +27,32 @@ exports.postSignup = (req, res, next) => {
     });
 };
 
-// exports.postSignin = (req, res, next) => {
-//   const email = req.body.email;
-//   const password = req.body.password;
+exports.postSignin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-//   bcrypt
-//     .hash(password, 12)
-//     .then(hashedPassword => {
-//       const user = new User({
-//         name: name,
-//         email: email,
-//         password: hashedPassword
-//       });
-//       return user.save();
-//     })
-//     .then(result => {
-//       res.status(200).json({ message: "Successfully sighup" });
-//     })
-//     .catch(err => {
-//       const error = new Error(err);
-//       error.httpStatusCode = 500;
-//       return next(error);
-//     });
-// };
+  User.findOne({ where: { email: email } })
+    .then(user => {
+      if (!user) {
+        return res.status(422).json({ message: "Invalid email or password" });
+      }
+
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            return res.status(200).json({ user: user });
+          }
+
+          throw err;
+        })
+        .catch(err => {
+          return res.status(422).json({ message: "Invalid email or password" });
+        });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,6 @@ const authController = require("../controllers/auth");
 
 router.post("/signup", authController.postSignup);
 
+router.post("/signin", authController.postSignin);
+
 module.exports = router;
