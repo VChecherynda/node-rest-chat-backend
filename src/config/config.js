58dd45c4941eba60b058bdc67ae9827a4d98613f
Vadym Checherinda <vadym.checherinda@litslink.com>
2020-05-03 02:32:41 +0300

    Update sequlaze

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const env = process.env.NODE_ENV || "development";
-
 export default {
-  [env]: {
+  development: {
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    operatorsAliases: false
+  },
+  staging: {
     username: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
