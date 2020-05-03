4ae23bbc8fb80f4f71121d00f80136e806002c07
Vadym Checherinda <vadym.checherinda@litslink.com>
2020-05-03 15:52:36 +0300

    Delete https server

--- src/app.js
+++ src/app.js
@@ -56,30 +56,7 @@ sequelize
   .sync()
   .then(result => {
     app.listen(port);
-    // https
-    //   .createServer(
-    //     {
-    //       key: fs.readFileSync("server.key"),
-    //       cert: fs.readFileSync("server.cert")
-    //     },
-    //     app
-    //   )
-    //   .listen(port, function() {
-    //     console.log(`App listening on port ${port}!`);
-    //   });
   })
   .catch(err => {
     console.log(err);
   });
-
-// https
-//   .createServer(
-//     {
-//       key: fs.readFileSync("server.key"),
-//       cert: fs.readFileSync("server.cert")
-//     },
-//     app
-//   )
-//   .listen(port, function() {
-//     console.log(`App 123 listening on port ${port}!`);
-//   });
